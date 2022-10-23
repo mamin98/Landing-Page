6826133351e934a97a8c00043d5e0cac399ab8f5
mamin98 <mohamedamin1421@gmail.com>
2022-10-23 17:11:04 +0200

    first fwd project

--- js/app.js
+++ js/app.js
@@ -5,17 +5,6 @@
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
  * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
 */
 
 /**
@@ -24,24 +13,13 @@
 */
 
 const fragment = document.createDocumentFragment(); // similar to <div> 
-const navList = document.querySelector('#navbar__list');
-const sections = document.querySelectorAll('section');
+const navList = document.getElementById('navbar-List');
+const allSections = document.getElementsByTagName('section'); // num of section in html page
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
 */
 
- const createAnchor = (id, name)=> {
-    
-    let htmltext = `<a class='menu__link' data-id='${id}'>${name}</a>`;
-    return htmltext;
-} 
-
-const nav_bar_position = section => {
-    return Math.floor(section.getBoundingClientRect().top);
-}
 
 /**
  * End Helper Functions
@@ -50,74 +28,66 @@ const nav_bar_position = section => {
 */
 
 // build the nav
-const create_Nav_Menu = () =>{
-    for(let section of sections){
-        const newItem = document.createElement('li');
-        const itemId = section.getAttribute('id');
-        const itemName = section.getAttribute('data-nav');
-        newItem.innerHTML = createAnchor(itemId, itemName);
+const create_navMenu = () =>{
 
-        fragment.appendChild(newItem);
+    for(let sec of allSections){
+        const listele = document.createElement('li');  
+        listele.innerHTML = `<a class='menu-link' href='#${sec.getAttribute('id')}'>
+        ${sec.getAttribute('class')}</a>`;
+       // put link in list item
+        fragment.appendChild(listele);
     }
-    const childList = document.getElementById('navbar__list');
-    childList.appendChild(fragment);
+    navList.appendChild(fragment);
 }
 
+ 
+ const queylistitem = document.querySelector('#navbar-List');
+
 // Add class 'active'
-const add_active_Class = (cond, sec) => {
+ const add_active_Class = (cond, sec) => {
     if(cond){
-        sec.classList.add('your-active-class');
-        sec.style.cssText= 'box-shadow: 20px 20px 50px 15px #0f3057; border:1px solid green; border-radius:1px';
+        sec.classList.toggle('your-active-class');
+        
+        sec.setAttribute('style', 
+        'box-shadow: 20px 20px 50px 15px #0f3057; border:1px solid green; border-radius:1px');
         const secId = sec.id.slice(7.8) -1;
-        navList.childNodes[secId].style.cssText = 'background-color: #fff';
+        queylistitem.childNodes[secId].setAttribute('style', 'background-color: #cc0');
     }
 }
 
 // Remove class 'active'
  const remove_active_Class = sec => {
-        sec.classList.remove('your-active-class');
-        sec.setAttribute('style',
+    sec.classList.toggle('your-active-class');
+    sec.setAttribute('style',
         'box-shadow: none; border:none; border-radius:none;');
         const secId = sec.id.slice(7.8) -1;
-        navList.childNodes[secId].setAttribute('style', 'background-color: none;');   
+        queylistitem.childNodes[secId].setAttribute('style', 'background-color: none;');   
 }
 
-// Scroll to anchor ID using scrollTO event
-
-const set_section_navbar = () =>{
-    for (let index = 0; index <sections.length; index++) {
-        const element = nav_bar_position(sections[index]);
-        display = () => element < 250 && element >= -250;
-
-        remove_active_Class(sections[index]);
-        add_active_Class(display(), sections[index]);
-    }
- }
-
-  const scroll_Element = (event) => {
-    if(event.target.nodeName === 'A'){
-        const secScroll = event.target.getAttribute('data-id');
-        const sec = document.getElementById(secScroll);
-        sec.scrollIntoView({
-            behavior: "smooth"
-        });
-    }
+const navbar_position = section => {
+    return section.getBoundingClientRect().top;
 }
+  
 
 /**
- * End Main Functions
- * Begin Events
- * 
+ * End Main Functions 
 */
 
-navList.addEventListener('click', function(e){
-    scroll_Element(e)
-  })
+ // Scroll to section on link click
+ this.addEventListener('scroll', set_secNavbar = () =>{
+    let ele = 0;
+    for (let index in allSections) {
+         ele = navbar_position(allSections[index]);
+         display = () => ele < 250 && ele >= -250;
 
-// Build menu 
+         remove_active_Class(allSections[index]);
+         add_active_Class(display(), allSections[index]);
+    }
+ }
+);
+
+
+// Build(call) menu 
   
-  create_Nav_Menu();
+create_navMenu();
 
-// Scroll to section on link click
-window.addEventListener('scroll' ,set_section_navbar);
-// Set sections as active
